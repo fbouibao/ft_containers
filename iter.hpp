0facdd510edbdc34f049533289a8066204f1fd52
Faical Bouibaouen <fbouibao@e1r6p11.1337.ma>
2022-02-19 19:13:46 +0100

    backup

--- iter.hpp
+++ iter.hpp
@@ -14,7 +14,7 @@ namespace ft{
 			typedef typename ft::iterator_traits<T>::pointer			pointer;
 			typedef typename ft::iterator_traits<T>::reference			reference;
 			typedef typename ft::iterator_traits<T>::difference_type	difference_type;
-			typedef typename ft::iterator_traits<T>::iterator_category  iterator_category;
+			typedef typename ft::iterator_traits<     >::iterator_category  iterator_category;
 
 
     private:
